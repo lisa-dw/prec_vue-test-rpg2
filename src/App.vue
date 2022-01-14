f09441cbd71102ac033b5bdfface8dc490a25201
seokjoon <seokjoon@gmail.com>
2022-01-14 12:03:15 +0900

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,89 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <input type="text" v-model="item.title" />
+    <br />
+    <input type="text" v-model="item.content" />
+    <br />
+    <button @click="createItem">create</button>
+    <br />
+    <input type="text" v-model="itemId">
+    <button @click="deleteItem">delete</button>
+    <button @click="updateItem">update</button>
+
+    <div>
+      <button @click="readItems">readsItems</button>
+<!--      {{ items }}-->
+      <div v-for="item in items" :key="item.id" :item="item">
+        {{ item.id }} || {{ item.title }} || {{ item.content }}
+      </div>
+    </div>
+
   </div>
 </template>
 
+
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
+
+const URL_API_FOO = 'http://localhost/api/foos'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+
+  data() {
+    return {
+      item: {
+        id: 0,
+        content: '',
+        title: '',
+      },
+      itemId: 0,
+      items: [],
+    }
+  },
+
+
+  methods: {
+    async createItem() {
+      const res = await axios.post(URL_API_FOO, { ...this.item })
+      console.log(res)
+    },
+
+    async deleteItem() {
+      const res = await axios.delete(URL_API_FOO + '/' + this.itemId)
+      console.log(res.data)
+    },
+
+    async readItems() {
+      const res = await axios.get(URL_API_FOO)
+      this.items = res.data
+      console.log(res)
+    },
+
+    updateItem() {
+      const res = axios.put(URL_API_FOO + '/' + this.itemId, {
+        item: this.item,
+      })
+      console.log(res)
+    },
+  },
+
+
+  watch: {
+    'item': {
+      deep: true,
+      handler() {
+        console.log('watch', this.item)
+      },
+    },
+  },
 }
 </script>
 
+
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
